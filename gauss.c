40833a575b2a9fa2632d7f8ea7ac922c3070b23a
Bernardo Duarte <bernardoduarte@tokenlab.com.br>
2017-11-07 19:07:23 -0200

    criação do arquivo C

---
+++ gauss.c
@@ -0,0 +1,15 @@
+/********************************************************
+*   Projeto 2 - Implementação Método de Gauss-Jordan    *
+*   12 Novembro 2017
+*                                                       *
+*   8598861 - Bernardo Simões Lage Gomes Duarte         *
+*   8936648 - Giovani Ortolani Barbosa                  *
+*   9066491 - Jorge Luiz da Silva Vilaça                *
+*   8937308 - Luiz Augusto Vieira Manoel                *
+********************************************************/
+
+
+int main (int argc, char **argv) {
+
+    return 1;
+}
