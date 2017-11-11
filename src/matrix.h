20d91fdfb2b50a724fc90e03e58809ab10c6d7d7
JorgeLuiz123321 <jorge.vilaca@usp.br>
2017-11-11 14:55:17 -0200

    funfando com vetor

--- src/matrix.h
+++ src/matrix.h
@@ -1,10 +1,10 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-float** read_matrix(int *dimension);
-float** create_matrix(int dimension);
-void destroy_matrix(int dimension, float **matrix);
-void print_matrix(int x, int y, float **m);
-void write_result(int dimension, float **matrix);
+float* read_matrix(int *dimension);
+float* create_matrix(int dimension);
+void destroy_matrix(float *matrix);
+void print_matrix(int dimension, float *m);
+void write_result(int dimension, float *matrix);
 
 #endif
